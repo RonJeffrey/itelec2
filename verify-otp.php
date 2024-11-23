d250663b08b8bc8dfde40d5a843ee31b5e0401b0
GutierrezMarvie <2020103878@dhvsu.edu.ph>
2024-11-23 13:05:53 +0800

    Update verify-otp.php

--- verify-otp.php
+++ verify-otp.php
@@ -7,6 +7,7 @@ include_once 'config/settings-configuration.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="src/img/PrimeStrength.png">
     <title>Verify OTP</title>
     <style>
         body {
